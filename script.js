5fe60e3b6548c02a1055614b75c60a23665d0861
stephan <stephan.pagel84@gmail.com>
2022-09-12 16:22:52 +0200

    ui connected

--- script.js
+++ script.js
@@ -16,16 +16,52 @@ const fuelCostsPerKm = (fuelCosts, distanceDriven) => {
   return (fuelCosts / distanceDriven).toFixed(2);
 };
 
-const mileageStart = 0;
-const mileageEnd = 600;
-const fuelQuantity = 35;
-const fuelCosts = 65;
+document.querySelector("#calc-button").addEventListener("click", function () {
+  const mileageStart = Number(
+    document.querySelector("#input-mileage-start").value
+  );
+  const mileageEnd = Number(document.querySelector("#input-mileage-end").value);
+  const fuelQuantity = Number(
+    document.querySelector("#input-fuel-quantity").value
+  );
+  const fuelCosts = Number(document.querySelector("#input-fuel-costs").value);
 
-const distance = distanceDriven(mileageStart, mileageEnd);
-const avgConsumption = fuelConsumptionPer100Km(distance, fuelQuantity);
-const costsPerLiter = fuelCostsPerLiter(fuelCosts, fuelQuantity);
-const costsPerKm = fuelCostsPerKm(fuelCosts, distance);
+  const distance = distanceDriven(mileageStart, mileageEnd);
+  const avgConsumption = fuelConsumptionPer100Km(distance, fuelQuantity);
+  const costsPerLiter = fuelCostsPerLiter(fuelCosts, fuelQuantity);
+  const costsPerKm = fuelCostsPerKm(fuelCosts, distance);
+  console.log(typeof costsPerKm, costsPerKm);
+  console.log(typeof costsPerLiter, costsPerLiter);
 
-console.log(
-  `Sie haben auf einer Strecke von ${distance} Kilometern ${avgConsumption} Liter Sprit verbraucht. \n Dies entspricht einem Durchschnittsverbrauch von ${avgConsumption} Litern auf 100 Kilometern. \n Für einen Liter Kraftstoff haben Sie ${costsPerLiter} € bezahlt. \n Für jeden gefahrenen Kilometer entstehen Ihnen Kosten in Höhe von ${costsPerKm} €.`
-);
+  if (!mileageStart) {
+    document.querySelector("#output-info").textContent =
+      "Bitte trage einen Kilometerstand Start ein.";
+  } else if (!mileageEnd) {
+    document.querySelector("#output-info").textContent =
+      "Bitte trage einen Kilometerstand Ende ein.";
+  } else if (!fuelQuantity) {
+    document.querySelector("#output-info").textContent =
+      "Bitte trage eine Spritmenge ein.";
+  } else if (!fuelCosts) {
+    document.querySelector("#output-info").textContent =
+      "Bitte trage deine Spritkosten ein.";
+  } else {
+    document.querySelector("#output-distance").textContent = distance;
+    document.querySelector("#output-fuel-quantity").textContent = fuelQuantity;
+    document.querySelector("#output-avg-consumption").textContent =
+      avgConsumption;
+    document.querySelector("#output-fuel-costs").textContent = fuelCosts;
+    document.querySelector("#output-costs-per-liter").textContent =
+      costsPerLiter;
+    document.querySelector("#output-costs-per-km").textContent = costsPerKm;
+  }
+});
+
+// const distance = distanceDriven(mileageStart, mileageEnd);
+// const avgConsumption = fuelConsumptionPer100Km(distance, fuelQuantity);
+// const costsPerLiter = fuelCostsPerLiter(fuelCosts, fuelQuantity);
+// const costsPerKm = fuelCostsPerKm(fuelCosts, distance);
+
+// console.log(
+//   `Sie haben auf einer Strecke von ${distance} Kilometern ${avgConsumption} Liter Sprit verbraucht. \n Dies entspricht einem Durchschnittsverbrauch von ${avgConsumption} Litern auf 100 Kilometern. \n Für einen Liter Kraftstoff haben Sie ${costsPerLiter} € bezahlt. \n Für jeden gefahrenen Kilometer entstehen Ihnen Kosten in Höhe von ${costsPerKm} €.`
+// );
